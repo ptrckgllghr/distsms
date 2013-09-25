5ab52caba4740e2bf0bbea0ecf70c3d19d96c111
adeclerk <adeclerk@home.highasdick.biz>
2013-09-25 18:06:49 -0400

    init

---
+++ distsms-client-desktop/src/distsms/client/desktop/DistsmsClientDesktop.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distsms.client.desktop;
+
+/**
+ *
+ * @author adeclerk
+ */
+public class DistsmsClientDesktop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
